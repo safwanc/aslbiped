15a2948dfbe79924aa9437cd9dce2be35d12aacb
Safwan Choudhury <im@safwanc.com>
2012-06-28 02:10:07 -0400

    Added support for FPE state machine

--- JacobianTransform.m
+++ JacobianTransform.m
@@ -1,14 +1,14 @@
-function [DQ, RESET] = JacobianTransform(MODE, BIPED, DX)
+function [DQ, RESET] = TaskController(MODE, STATE, BIPED, DX)
 %#codegen
 
-    DQ = zeros(14,1); 
-    RESET = 0; 
+    DQ = zeros(14,1); RESET = 0;
 
     % Controller State Variables
     persistent LAST
     if isempty(LAST)
         LAST = struct( ...
-            'MODE', MODE ...
+            'MODE', MODE, ...
+            'STATE', STATE ...
             ); 
     end
 
@@ -21,21 +21,32 @@ function [DQ, RESET] = JacobianTransform(MODE, BIPED, DX)
     if isempty(S)   S = [eye(14) zeros(14, 6)]; end
     if isempty(JB) JB = [zeros(6,14) eye(6)];   end
     
-    if MODE ~= 0
+    if STATE ~= FPEState.StandStill
+        
+        [ SWING, STAND ] = ParseState(STATE);
+        
         [ Jcom_xy, Jcom_z ] = SplitJcom(Jcom(BIPED)); 
         DXcom_xy = DX(1:2); DXcom_z = DX(3); 
 
-        Jbase_rpy = JB(4:6,:); 
-        DXbase_rpy = DX(4:6); 
+        % Jbase_rpy = JB(4:6,:);  
+        % DXbase_rpy = DX(4:6); 
 
-        Jswing = Jleg(L, BIPED.L, BIPED.TW0);
-        DXswing = DX(7:12); 
+        Jswing = Jleg(SWING, BIPED);
+        
+        switch(SWING)
+            case 1
+                DXswing = DX(7:12); 
+            case 2
+                DXswing = DX(13:18); 
+            otherwise
+                DXswing = zeros(6,1); 
+        end
 
         %% High Priority Tasks
         Jhigh   = [Jcom_xy; Jswing];
         DXhigh  = [DXcom_xy; DXswing];  
 
-        [Jh, DXh] = Jconstraint(R, BIPED, Jhigh, DXhigh); 
+        [Jh, DXh] = Jconstraint(STAND, BIPED, Jhigh, DXhigh); 
 
         %% Low Priority Tasks
         Jl  = Jcom_z; 
@@ -45,11 +56,32 @@ function [DQ, RESET] = JacobianTransform(MODE, BIPED, DX)
         DQ(:,:) = S * Prioritized(Jh, DXh, Jl, DXl); 
     end
     
-    if MODE ~= LAST.MODE
-        RESET = 1; 
+    if STATE ~= LAST.STATE
+        if ((STATE == FPEState.StandStill) || ...
+                (LAST.STATE == FPEState.StandStill))
+            RESET = 1; 
+        end
     end
+
+    %CSTATE = STATE; % ugly hack to synchronize CM switching
     
     LAST.MODE = MODE; 
+    LAST.STATE = STATE; 
+end
+
+function [ SWING, STAND ] = ParseState(STATE)
+
+    switch(STATE)
+        case {STATE.LeftPush, STATE.LeftLift, STATE.LeftSwing, STATE.LeftDrop}
+            SWING = 1; 
+            STAND = 2; 
+        case {STATE.RightPush, STATE.RightLift, STATE.RightSwing, STATE.RightDrop}
+            SWING = 2; 
+            STAND = 1; 
+        otherwise
+            error('Unsupported State'); 
+    end
+
 end
 
 function [ DQ ] = Prioritized(J1, X1, J2, X2)
@@ -61,36 +93,31 @@ end
 function [ Jc, DXc ] = Jconstraint(SIDE, BIPED, J, DX)
     
     DXs = zeros(6,1);
+    Js = Jleg(SIDE, BIPED); 
     
      switch(SIDE)
          
         case 1  % Left Foot Single Support 
-            Js = Jleg(1, BIPED.L, BIPED.TW0); 
             if ~InContact(BIPED.R.FOOT)
                 DXs = [BIPED.L.FOOT.V; BIPED.L.FOOT.W]; 
             end
             
         case 2 % Right Foot Single Support 
-            Js = Jleg(2, BIPED.R, BIPED.TW0); 
+            
             if ~InContact(BIPED.L.FOOT)
                 DXs = [BIPED.R.FOOT.V; BIPED.R.FOOT.W]; 
             end
             
          otherwise
+             
              error('Unsupported'); 
-            
-            %DXs = [BIPED.R.FOOT.V; BIPED.R.FOOT.W]; 
-%         otherwise % Double Support  
+             
 %             Js1 = Jleg(1, BIPED.L, BIPED.TW0);
 %             Js2 = Jleg(2, BIPED.R, BIPED.TW0);
 %             Js = [Js1(1:6,:); Js2(1:6,:)]; 
 %             DXs = [...
 %                 BIPED.L.FOOT.V; BIPED.L.FOOT.W; ...
 %                 BIPED.R.FOOT.V; BIPED.R.FOOT.W; ...
-%                 ]; 
-%             DXs = [...
-%                 BIPED.L.FOOT.V; zeros(3,1); ...
-%                 BIPED.R.FOOT.V; zeros(3,1); ...
 %                 ]; 
 %             
             % DXs = zeros(size(Js,1),1);
@@ -197,29 +224,33 @@ end
 % END-EFFECTOR JACOBIANS
 % ------------------------------------------------------------------------
 
-function [ J ] = Jleg(SIDE, LEG, TW0)
-    J = zeros(6,20); 
-    RB = TW0(1:3,1:3);
-    Z = RB * LEG.Z; %LEG.Z; 
-    O = TransformArray(TW0, LEG.O); %LEG.O; 
-    EE = Transform(TW0, LEG.XF); 
+function [ J ] = Jleg(SIDE, BIPED)
 
-%     Z = LEG.Z; 
-%     O = LEG.O; 
-%     EE = LEG.XF; 
+    J = zeros(6,20); 
+
+    switch(SIDE)
+        case 1 
+            LEG = BIPED.L;
+        case 2 
+            LEG = BIPED.R; 
+        otherwise
+            error('Invalid side'); 
+    end
+    
+    Z = BIPED.TW0(1:3,1:3) * LEG.Z;         %     Z = LEG.Z; 
+    O = TransformArray(BIPED.TW0, LEG.O);   %     O = LEG.O; 
+    EE = Transform(BIPED.TW0, LEG.XF);      %     EE = LEG.XF; 
     
     switch(SIDE)
         case 1 
-            % Left Leg
             J(:,1:7) = JLi(Z, O, EE); 
         case 2 
-            % Right Leg
             J(:,8:14) = JLi(Z, O, EE); 
         otherwise
             error('Invalid side'); 
     end
      
-    J(:,15:20) = Jb(TW0, LEG.XF); 
+    J(:,15:20) = Jb(BIPED.TW0, LEG.XF); 
 end
 
 function [ J ] = JLi(Z, O, EE)
