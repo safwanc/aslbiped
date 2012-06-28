cbb1a51c3ce31c27355941e2290d6aea75a040a4
Safwan Choudhury <im@safwanc.com>
2012-06-28 04:08:52 -0400

    Preliminary work to handle LeftPush state

--- TrajectoryGenerator.m
+++ TrajectoryGenerator.m
@@ -1,50 +1,101 @@
-function [ XCOM, XLP, XRP ] = TrajectoryGenerator( STATE, COM, FPEX, LP, RP )
-%#codegen
+function [ XCOM, XLP, XRP ] = ...
+    TrajectoryGenerator( STATE, COM, FPEX, LP, RP, ...
+    STEPL, STEPW, COMH, GNDH, GNDCLR)
+    %#codegen
 
-    persistent BETA STEPL STEPW COMH GNDH GNDCLR
-    if isempty(BETA) 
-        BETA = 24 * pi/180; 
+    % Generator State Variables
+    persistent LAST
+    if isempty(LAST)
+        LAST = struct( ...
+            'STATE', STATE, ...
+            'XCOM', [0.0329 -0.0198 0.402]', ...
+            'XLP', [-0.0273 -0.1292 -0.001627]', ...
+            'XRP', [-0.02575 0.13 -0.002]' ...
+            ); 
     end
     
-    if isempty(STEPL)
-        STEPL = 0.17; 
+    if (STATE ~= LAST.STATE)
+        %
+        
+        % [ SWING, STAND ] = ParseState(STATE, LP, RP);
+        SWING = LP; STAND = RP; 
+        
+        switch(STATE)
+            case {FPEState.LeftPush, FPEState.RightPush}
+                XCOM = [STAND(1) STAND(2) COMH]'; 
+            otherwise
+                error('Unsupported State'); 
+        end
+        
+        XLP = SWING; XRP = STAND; 
+        % [ XLP, XRP ] = SetState(STATE, SWING, STAND);
+        
+        LAST.XCOM = XCOM; 
+        LAST.XLP = XLP; 
+        LAST.XRP = XRP; 
+        
+    else
+        
+        % @TODO: Check if in DROP state (to keep updating swing foot traj
+        %           in order to track FPE.
+        
+        XCOM = LAST.XCOM; 
+        XLP = LAST.XLP; 
+        XRP = LAST.XRP; 
     end
+
+    LAST.STATE = STATE;
+
+end
+
+%     XCOM = [0.0329 -0.0198 0.402]'; 
+%     XLP = [-0.0273 -0.1292 -0.001627]';
+%     XRP = [-0.02575 0.13 -0.002]'; 
     
-    if isempty(STEPW)
-        STEPW = 0.1640; 
-    end
-    
-    if isempty(COMH)
-        COMH = 0.4; 
-    end
-    
-    if isempty(GNDH)
-        GNDH = -0.002; 
-    end
-    
-    if isempty(GNDCLR)
-        GNDCLR = 0.07; 
-    end
-    
-    % @TODO
-    SWING = LP; 
-    STAND = RP; 
-    
+%     [ SWING, STAND ] = ParseState(STATE, LP, RP);
+%     
+%     switch(STATE)
+%         case PUSH
+%             XCOM = [STAND(1) STAND(2) COMH]'; 
+%         case LIFT
+%             XSWING = [STAND(1) SWING(2) GNDCLR]'; 
+%         case SWING
+%             XSWING = [SWING(1)+STEPL SWING(2) GNDCLR]'; 
+%         case DROP
+%             XSWING = [FPEX STAND(2)+STEPW GNDH]';
+%             XCOM = [STAND(1)+(STEPL/2) STAND(2)+(STEPW/2) COMH]'; 
+%         otherwise
+%             error('Unsupported State'); 
+%     end
+%     
+%     [ XLP, XRP ] = SetState(STATE, SWING, STAND)
+
+function [ SWING, STAND ] = ParseState(STATE, LP, RP)
+
     switch(STATE)
-        case PUSH
-            XCOM = [STAND(1) STAND(2) COMH]'; 
-        case LIFT
-            XSWING = [STAND(1) SWING(2) GNDCLR]'; 
-        case SWING
-            XSWING = [SWING(1)+STEPL SWING(2) GNDCLR]'; 
-        case DROP
-            XSWING = [FPEX STAND(2)+STEPW GNDH]';
-            XCOM = [STAND(1)+(STEPL/2) STAND(2)+(STEPW/2) COMH]'; 
+        case {1, 2, 3, 4}
+            SWING = LP; 
+            STAND = RP; 
+        case {5, 6, 7, 8}
+            SWING = RP; 
+            STAND = LP; 
         otherwise
             error('Unsupported State'); 
     end
-    
-    XLP = XSWING; 
-    XRP = RP; 
+
+end
+
+function [ LP, RP ] = SetState(STATE, SWING, STAND)
+
+    switch(STATE)
+        case {1, 2, 3, 4}
+            LP = SWING; 
+            RP = STAND; 
+        case {5, 6, 7, 8}
+            RP = SWING; 
+            LP = STAND; 
+        otherwise
+            error('Unsupported State'); 
+    end
 
 end
